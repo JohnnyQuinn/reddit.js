ed85db7db579ca793dd5bd4bb491be6522a7d457
johnnyquinn <jftq97@gmail.com>
2021-05-08 16:28:14 -0400

    Rendered homepage with handlebars successfully

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 import cors from 'cors'
 
 const express = require('express')
+const exphbs  = require('express-handlebars');
 const app = express()
 const port = 3000
 
-app.use(cors())
+//app setup
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`server.js listening at http://localhost:${port}`)
 })
